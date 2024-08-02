61328857330641cb72a2f92d804c5e4bc654c566
Santiago Melo <santiagocano15cr7@gmail.com>
2024-08-01 21:32:39 -0600

    dependecies update and generic

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        staleTimes: {
+            dynamic: 30,
+        },
+    },
+}
 
 module.exports = nextConfig
