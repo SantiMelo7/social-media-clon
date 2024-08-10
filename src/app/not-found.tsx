2e16de3ee7c0aaf70e3c13e74520db8b91a2d639
Santiago Melo <santiagocano15cr7@gmail.com>
2024-08-09 22:10:09 -0600

    generic

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="notFoundContainer">
+            <h1 className="notFoundTitle">Not Found</h1>
+            <p>Yhis page you are loading for does not exist.</p>
+        </main>
+    )
+}
