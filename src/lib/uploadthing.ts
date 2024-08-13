002249a4264451c1457c5b8c14f3dec5039bbf61
Santiago Melo <santiagocano15cr7@gmail.com>
2024-08-12 18:22:02 -0600

    generic

---
+++ src/lib/uploadthing.ts
@@ -0,0 +1,4 @@
+import { AppFileRoute } from "@/app/api/uploadthing/core"
+import { generateReactHelpers } from "@uploadthing/react"
+
+export const { useUploadThing, uploadFiles } = generateReactHelpers<AppFileRoute>()
