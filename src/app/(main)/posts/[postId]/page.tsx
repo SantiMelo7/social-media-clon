55ff2a9794bc570b2a394748c253ee032b32987c
Santiago Melo <santiagocano15cr7@gmail.com>
2024-08-14 20:17:22 -0600

    build correct

--- src/app/(main)/posts/[postId]/page.tsx
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -1,15 +1,10 @@
 import Loading from "@/app/loading"
 import { validateRequest } from "@/auth"
-import FollowButton from "@/components/follow/FollowButton"
-import LinkiFy from "@/components/linkify/LinkiFy"
 import Post from "@/components/posts/Post"
-import UserAvatar from "@/components/users/UserAvatar"
-import UserTooltip from "@/components/users/UserTooltip"
-import { UserDataProps } from "@/interfaces/userData"
+import UserInfoSide from "@/components/users/UserInfoSide"
 import prisma from "@/lib/prisma"
 import { getPostDataInclude } from "@/lib/types"
 import { Metadata } from "next"
-import Link from "next/link"
 import { notFound } from "next/navigation"
 import { cache, Suspense } from "react"
 
@@ -62,40 +57,5 @@ export default async function PostPage({ params: { postId } }: PostPageProps) {
                 </Suspense>
             </div>
         </main>
-    )
-}
-
-export async function UserInfoSide({ user }: UserDataProps) {
-    const { user: loggedUser } = await validateRequest()
-    await new Promise(r => setTimeout(r, 4000))
-    if (!loggedUser) return
-    return (
-        <div className="space-y-5 rounded-xl bg-card p-5 shadow-sm">
-            <div className="text-2xl font-bold">About this user</div>
-            <UserTooltip user={user}>
-                <Link href={`/users/${user.username}`} className="flex items-center gap-3">
-                    <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
-                    <div>
-                        <p className="line-clamp-1 break-all font-semibold hover:underline">
-                            {user.displayName}
-                        </p>
-                        <p className="line-clamp-1 break-all text-muted-foreground">
-                            @{user.username}
-                        </p>
-                    </div>
-                </Link>
-            </UserTooltip>
-            <LinkiFy>
-                <div className="line-clamp-6 whitespace-pre-line break-words text-muted-foreground">
-                    {user.bio}
-                </div>
-            </LinkiFy>
-            {user.id !== loggedUser.id && (
-                <FollowButton userId={user.id} initialState={{
-                    followers: user._count.followers,
-                    isFollowedByUser: user.followers.some(({ followerId }) => followerId === loggedUser.id)
-                }} />
-            )}
-        </div>
     )
 }
